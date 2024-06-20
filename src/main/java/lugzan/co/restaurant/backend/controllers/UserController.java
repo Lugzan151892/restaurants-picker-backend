f5bb12965cbc4ba8ca18e91b43dffbe0fd26144d
Denis Gukov <denis.guk@konomic.com>
2024-06-20 19:07:34 +0300

    api/users: fixed add user request, added add user response, fixed hash password

--- src/main/java/lugzan/co/restaurant/backend/controllers/UserController.java
+++ src/main/java/lugzan/co/restaurant/backend/controllers/UserController.java
@@ -1,32 +1,48 @@
 package lugzan.co.restaurant.backend.controllers;
 
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import lugzan.co.restaurant.backend.models.UserModel;
 import lugzan.co.restaurant.backend.repository.UserRepository;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
-@RequestMapping(path="/api")
+@RequestMapping(path="/api/user")
 public class UserController {
     @Autowired
 
     private UserRepository userRepository;
 
     @PostMapping(path="/add")
-    public @ResponseBody String addNewUser (@RequestParam String name, @RequestParam String email) {
+    public @ResponseBody String addNewUser (@RequestBody UserModel user) {
+        System.out.println(user);
+
+
         UserModel n = new UserModel();
-        n.setUserName(name);
-        n.setEmail(email);
+        n.setUserName(user.getUserName());
+        n.setEmail(user.getEmail());
+        n.setPassword(user.getPassword());
         userRepository.save(n);
-        return "Saved";
+
+        JSONObject response = new JSONObject();
+        JSONObject data = new JSONObject();
+
+        data.put("userName", n.getUserName());
+        data.put("email", n.getEmail());
+        response.put("error", false);
+        response.put("status", 200);
+        response.put("data", data);
+
+        return response.toString();
     }
 
-    @GetMapping(path="/all")
+    @GetMapping(path = "/all")
     public @ResponseBody Iterable<UserModel> getAllUsers() {
         return userRepository.findAll();
     }
