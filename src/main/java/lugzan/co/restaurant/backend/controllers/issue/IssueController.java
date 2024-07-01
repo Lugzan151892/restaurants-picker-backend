23514107aa95e7f70e0c446ba47569dda415f42f
Denis Gukov <denis.guk@konomic.com>
2024-07-01 22:13:07 +0300

    api/issues: trying to fix error

--- src/main/java/lugzan/co/restaurant/backend/controllers/issue/IssueController.java
+++ src/main/java/lugzan/co/restaurant/backend/controllers/issue/IssueController.java
@@ -1,7 +1,6 @@
 package lugzan.co.restaurant.backend.controllers.issue;
 
 import io.jsonwebtoken.Claims;
-import lugzan.co.restaurant.backend.controllers.user.UserController;
 import lugzan.co.restaurant.backend.models.issue.IssueCreateModel;
 import lugzan.co.restaurant.backend.models.issue.IssueListModel;
 import lugzan.co.restaurant.backend.models.issue.IssueModel;
@@ -11,7 +10,6 @@ import lugzan.co.restaurant.backend.repository.UserRepository;
 import lugzan.co.restaurant.backend.services.ApiErrorMessageEnums;
 import lugzan.co.restaurant.backend.services.ApiService;
 import lugzan.co.restaurant.backend.services.JwtService;
-import lugzan.co.restaurant.backend.services.issue.enums.IssueStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -25,41 +23,43 @@ public class IssueController {
     @Autowired
     private IssueRepository issueRepository;
     final private ApiService apiService = new ApiService();
-    final private UserController userController = new UserController();
 
-    @PostMapping(path = "/create")
-    public @ResponseBody String create(@RequestHeader(value = "Authorization", required = false) String token, @RequestBody IssueRequest issue) {
-
-        String tokenError = JwtService.getTokenError(token);
-
-        if (!tokenError.isEmpty()) {
-            apiService.setStatus(400);
-            return apiService.createMessageResponse(tokenError);
-        }
-
-        Claims tokenData = JwtService.getTokenData(JwtService.getSubToken(token));
-        String userName = tokenData.getSubject();
-        UserModel user = userRepository.findByUserName(userName);
-
-        if (user == null) {
-            apiService.setStatus(400);
-            return apiService.createErrorResponse(ApiErrorMessageEnums.USER_NOT_FOUND, userName);
-        }
-
-        if (
-            issue.getTitle() == null || issue.getTitle().isEmpty()
-            || issue.getDescription() == null || issue.getDescription().isEmpty()
-        ) {
-            apiService.setStatus(400);
-            return apiService.createMessageResponse("Title and description are required");
-        }
-
-        IssueModel newIssue = new IssueModel(issue.getTitle(), issue.getDescription(), issue.getPriority(), user);
-        issueRepository.save(newIssue);
-        IssueCreateModel data = new IssueCreateModel(newIssue.getId());
-        apiService.setStatus(200);
-        return apiService.createSuccessResponse(data);
-    }
+//    @PostMapping(path = "/create")
+//    public @ResponseBody String create(@RequestHeader(value = "Authorization", required = false) String token, @RequestBody IssueRequest issue) {
+//        if (token == null || token.isEmpty()) {
+//            apiService.setStatus(400);
+//            return apiService.createErrorResponse(ApiErrorMessageEnums.TOKEN_INCORRECT, "");
+//        }
+//
+//        String subToken = token.substring(7);
+//
+//        if (JwtService.isTokenExpired(subToken)) {
+//            apiService.setStatus(400);
+//            return apiService.createErrorResponse(ApiErrorMessageEnums.TOKEN_EXPIRED, "");
+//        }
+//        Claims tokenData = JwtService.getTokenData(JwtService.getSubToken(subToken));
+//        String userName = tokenData.getSubject();
+//        UserModel user = userRepository.findByUserName(userName);
+//
+//        if (user == null) {
+//            apiService.setStatus(400);
+//            return apiService.createErrorResponse(ApiErrorMessageEnums.USER_NOT_FOUND, userName);
+//        }
+//
+//        if (
+//            issue.getTitle() == null || issue.getTitle().isEmpty()
+//            || issue.getDescription() == null || issue.getDescription().isEmpty()
+//        ) {
+//            apiService.setStatus(400);
+//            return apiService.createMessageResponse("Title and description are required");
+//        }
+//
+//        IssueModel newIssue = new IssueModel(issue.getTitle(), issue.getDescription(), issue.getPriority(), user);
+//        issueRepository.save(newIssue);
+//        IssueCreateModel data = new IssueCreateModel(newIssue.getId());
+//        apiService.setStatus(200);
+//        return apiService.createSuccessResponse(data);
+//    }
 
 //    @PostMapping(path = "edit")
 //    public @ResponseBody String edit(@RequestHeader(value = "Authorization", required = false) String token, @RequestBody IssueRequest issue) {
